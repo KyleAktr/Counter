79d189325ecd593608128bc85bcc544248369515
KyleAktr <aktr.contact@gmail.com>
2023-06-07 17:10:17 +0200

    00000 + letter spacing

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ let counterElement = document.getElementById("counter");
 
 function updateCounter() {
   counter++;
-  counterElement.textContent = counter;
+  let formattedCounter = counter.toString().padStart(5, "0");
+  counterElement.textContent = formattedCounter;
 }
+
+window.addEventListener("DOMContentLoaded", function () {
+  let formattedCounter = counter.toString().padStart(5, "0");
+  counterElement.textContent = formattedCounter;
+});
 
 document.addEventListener("keydown", updateCounter);
